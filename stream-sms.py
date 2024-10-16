2838a73112c12df47bb36631912b21aa613de34c
GilangShaputrDwi <122088277+GilangShaputrDwi@users.noreply.github.com>
2024-10-16 15:15:24 +0700

    Update stream-sms.py

--- stream-sms.py
+++ stream-sms.py
@@ -143,6 +143,5 @@ if st.button('Hasil Deteksi'):
 
         # Kosongkan input teks setelah menampilkan hasil
         st.session_state['input_text'] = ''
-        st.experimental_rerun()  # Refresh halaman untuk mengosongkan input
     else:
         st.warning("Silakan masukkan teks SMS sebelum melakukan prediksi.")
